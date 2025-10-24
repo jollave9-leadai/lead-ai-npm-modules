b94cedc5b53d31f7b80b374166f20459a1407a25
Jonathan Ollave <jonathan.o@leadai.com.au>
2025-10-24 14:19:40 +0800

    publish build

---
+++ src/types.ts
@@ -0,0 +1,56 @@
+export interface VapiIntegration {
+    firstMessageMode: string;
+    backgroundSound: string;
+    serverUrl: string;
+    transcriber: {
+      provider: string;
+      model: string;
+      language: string;
+    };
+    voice: {
+      provider: string;
+      model: string;
+      language: string;
+    };
+    chunkPlan: {
+      waitSeconds: number;
+      smartEndpointingEnabled: boolean;
+    };
+    stopSpeakingPlan: {
+      voiceSeconds: number;
+      numWords: number;
+    };
+    messagePlan: {
+      waitSeconds: number;
+      smartEndpointingEnabled: boolean;
+    };
+    clientMessages: {
+      message: string;
+      waitSeconds: number;
+    }[];
+    serverMessages: {
+      message: string;
+      waitSeconds: number;
+    }[];
+    phoneNumberId: string;
+    phoneNumber: string;
+    client_id: string;
+    model_configurations: {
+      providers: {
+        name: string;
+      };
+      model: string;
+    };
+    tools: {
+      name: string;
+      description: string;
+    }[];
+    toolIds: string[];
+    endCallPhrases: string[];
+    startSpeakingPlan: {
+      waitSeconds: number;
+      smartEndpointingEnabled: boolean;
+    };
+    temperature: number;
+    maxToken: number;
+  }
